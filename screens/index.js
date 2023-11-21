25eb35fc12f3102ea70b9b0397d0b73d1045a05a
wahyuex <88044555+wahyuex@users.noreply.github.com>
2023-11-22 05:19:52 +0700

    done modul 10 apl todolist dasar

---
+++ screens/index.js
@@ -0,0 +1,7 @@
+
+import AboutScreen from "./about";
+import TaskScreen from "./task-all";
+import TaskCompletedScreen from "./task-completed";
+
+export { AboutScreen, TaskCompletedScreen, TaskScreen };
+
